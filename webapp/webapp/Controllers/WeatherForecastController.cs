7a87ad68d4657bc04c1f6e726344628bd772ae86
Eduardo <eduardotakeuchi@outlook.com>
2022-06-14 15:40:53 -0300

    Upload project with communication mongoDB

---
+++ webapp/webapp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace webapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipwreck> Get()
+        {
+            var client = new MongoClient("mongodb+srv://root:<masterkey>@cluster0.oaocbkx.mongodb.net/?retryWrites=true&w=majority");
+            var database = client.GetDatabase("sample_geospatial");
+            var collection = database.GetCollection<Shipwreck>("shipwrecks");
+            return collection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+
+        }
+    }
+}
---
+++ webapp/webapp/Shipwreck.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace webapp
+{
+    [BsonIgnoreExtraElements]
+    public class Shipwreck
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("feature_type")]
+        public string FeatureType { get; set; }
+        [BsonElement("chart")]
+        public string Chart { get; set; }
+        [BsonElement("latdec")]
+        public double Latitude { get; set; }
+        [BsonElement("londec")]
+        public double Longitude { get; set; }
+
+    }
+}
